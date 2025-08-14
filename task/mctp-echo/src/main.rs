ec8ee09d3d082a9dd3d271c77f3772ec538e0255
Marvin Gudel <marvin.gudel@9elements.com>
2025-08-14 15:06:50 +0200

    Basic MCTP echo example task

---
+++ task/mctp-echo/src/main.rs
@@ -0,0 +1,38 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! A demo task that echoes MCTP messages.
+//!
+//! The task configures the MCTP server for EID 8
+//! and starts listening for MCTP Message Type `1` (PLDM) packets.
+//! Received messages are echoed as is through the response channel.
+
+#![no_std]
+#![no_main]
+
+use mctp::{Eid, Listener, MsgType, RespChannel};
+use mctp_api::{Router, RouterListener};
+use userlib::*;
+
+task_slot!(MCTP, mctp);
+
+#[export_name = "main"]
+fn main() -> ! {
+    let router = mctp_api::Router::from(mctp);
+
+    router.set_eid(Eid(8)).unwrap_lite();
+    let listener = router.listener(MsgType(1)).unwrap_lite();
+    let mut recv_buf = [0; 255];
+
+    loop {
+        let (_, _, msg, resp) = listener.recv(&mut recv_buf).unwrap_lite();
+
+        match resp.send(buf) {
+            Ok(_) => {}
+            Err(e) => {
+                // Error sending response to peer
+            }
+        }
+    }
+}
